1cbed562b40dee6c44176a4e1e746908dd6b65ad
silvershade1 <silvershade1000@gmail.com>
2024-11-19 20:18:09 +0330

    feat : add interpreter

--- main.ts
+++ main.ts
@@ -1,18 +1,22 @@
 import Parser from "./frontend/parser.ts";
-
+import { evaluate } from "./runtime/interpreter.ts";
 repl();
 
 function repl() {
   const parser = new Parser();
   console.log("\nRepl v0.1");
 
+  // Continue Repl Until User Stops Or Types `exit`
   while (true) {
     const input = prompt("> ");
     if (!input || input.includes("exit")) {
       Deno.exit(1);
     }
 
+    // Produce AST From sourc-code
     const program = parser.produceAST(input);
-    console.log(program);
+
+    const result = evaluate(program);
+    console.log(result);
   }
 }
